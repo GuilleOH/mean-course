efc17cb9783078172e3f6347e3a176aedb91701d
GUILLERMO ORTIZ HERRERA (GRH) <guillermo.ortiz@taxo.es>
2019-02-12 13:13:40 +0100

    Curso acabado

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/MeanCourse')
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended: false}));
 
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req,res, next)=>{
   res.setHeader('Access-Control-Allow-Origin','*');
